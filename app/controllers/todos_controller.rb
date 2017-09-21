1cc3cbbe2abeda6c93de054652d4c9213db0ecb1
James Hughes <james.a.hughes@gmail.com>
2017-09-20 19:11:37 -0500

    Add todos API create, update and destroy actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,9 +5,29 @@ class TodosController < ApplicationController
     # render json: Todo.all
   end
 
+  def create
+    @todo = Todo.new(
+      text: params[:text],
+      is_completed: params[:is_completed]
+    )
+    if @todo.save
+      render "show.json.jbuilder"
+    else
+      render json: { errors: @todo.errors.full_messages }, status: 422
+    end
+  end
+
   def show
     @todo = Todo.find_by(id: params[:id])
     render "show.json.jbuilder" 
+  end
+
+  def update
+    @todo = Todo.find_by(id: params[:id])
+    @todo.text = params[:text] || @todo.text
+    @todo.is_completed = params[:is_completed] || @todo.is_completed
+    @todo.save
+    render "show.json.jbuilder"
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/todos" => "todos#index"
   get "/todos/:id" => "todos#show"
+  post "/todos" => "todos#create"
+  patch "/todos/:id" => "todos#update"
+  delete "/todos/:id" => "todos#destroy"
 end
 
